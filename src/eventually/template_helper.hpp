7c2c40faafe54f3089291f9043a4d13742c35d5b
Miguel Ibero <miguel@ibero.me>
2014-07-20 13:25:53 +0200

    removed tabs

--- src/eventually/template_helper.hpp
+++ src/eventually/template_helper.hpp
@@ -7,23 +7,23 @@ namespace eventually {
 
     namespace template_helper
     {
-		template <class T1, class ...T>
-		struct first
-		{
-		    typedef T1 type;
-		};
+        template <class T1, class ...T>
+        struct first
+        {
+            typedef T1 type;
+        };
 
-		template <class T1, class ...T>
-		struct last
-		{
-		    typedef typename last<T...>::type type;
-		};
+        template <class T1, class ...T>
+        struct last
+        {
+            typedef typename last<T...>::type type;
+        };
 
-		template <class T1>
-		struct last<T1>
-		{
-		    typedef T1 type;
-		};
+        template <class T1>
+        struct last<T1>
+        {
+            typedef T1 type;
+        };
     }
 
 }
