2711a0687d37ede43595cff942ab62db1eb25fb9
Miguel Ibero <miguel.ibero@socialpoint.es>
2014-08-13 12:35:46 +0200

    moved is_any to is_same

--- src/eventually/is_same.hpp
+++ src/eventually/is_same.hpp
@@ -1,6 +1,6 @@
 
-#ifndef _eventually_is_any_hpp_
-#define _eventually_is_any_hpp_
+#ifndef _eventually_is_same_hpp_
+#define _eventually_is_same_hpp_
 
 #include <type_traits>
 
@@ -11,14 +11,14 @@ namespace eventually {
      * http://stackoverflow.com/questions/17032310/how-to-make-a-variadic-is-same
      */
     template<typename T, typename... Rest>
-    struct is_any : std::false_type {};
+    struct is_same : std::false_type {};
 
     template<typename T, typename First>
-    struct is_any<T, First> : std::is_same<T, First> {};
+    struct is_same<T, First> : std::is_same<T, First> {};
 
     template<typename T, typename First, typename... Rest>
-    struct is_any<T, First, Rest...>
-        : std::integral_constant<bool, std::is_same<T, First>::value || is_any<T, Rest...>::value>
+    struct is_same<T, First, Rest...>
+        : std::integral_constant<bool, std::is_same<T, First>::value && is_same<T, Rest...>::value>
     {};
 
 }
